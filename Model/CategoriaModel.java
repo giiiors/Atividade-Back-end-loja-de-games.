2178891c0e88c5d90ee11954495d46c47c566e21
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-05 14:55:22 -0300

    mais um

--- Model/CategoriaModel.java
+++ Model/CategoriaModel.java
@@ -1,5 +1,18 @@
 package com.generation.lojaDeGames.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tb_categoria")
+
 public class CategoriaModel {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
 
 }
