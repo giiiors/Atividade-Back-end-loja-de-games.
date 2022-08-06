65c42d779168fa0c3db51dd6183299c113517516
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-06 20:50:35 -0300

    vai

--- Model/ProdutoModel.java
+++ Model/ProdutoModel.java
@@ -1,5 +1,62 @@
 package com.generation.lojaDeGames.Model;
 
-public class ProdutoModel {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_produto")
+
+public class ProdutoModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	private String nomeProduto;
+	
+	private int quantidade;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public CategoriaModel getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaModel categoria) {
+		this.categoria = categoria;
+	}
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private CategoriaModel categoria;
+	
 }
--- Repository/ProdutoRepository.java
+++ Repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package com.generation.lojaDeGames.Repository;
 
-public class ProdutoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.lojaDeGames.Model.ProdutoModel;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
 
 }
