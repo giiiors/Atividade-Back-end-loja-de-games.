c85e1dad368352aca515236c68f9daab4848dd15
Gio Ferraz <107887069+giiiors@users.noreply.github.com>
2022-08-06 21:14:07 -0300

    uai

--- Controller/LojadegamesController.java
+++
@@ -1,5 +0,0 @@
-package com.generation.lojaDeGames.Controller;
-
-public class LojadegamesController {
-
-}
--- Repository/ProdutoRepository.java
+++ Repository/ProdutoRepository.java
@@ -1,11 +1,17 @@
 package com.generation.lojaDeGames.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.lojaDeGames.Model.ProdutoModel;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
+public List <ProdutoModel> findProdutoBetween (@Param("inicio") BigDecimal inicio, @Param ("fim") BigDecimal fim);
+	
 
 }
